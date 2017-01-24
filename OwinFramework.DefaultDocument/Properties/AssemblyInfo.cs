1ac56a45c377a8b810a5e6e2172a9cc81639d07e
Martin Halliday <martin.bikeman@gmail.com>
2017-01-23 20:54:36 -0800

    Released to NuGet mostly 2.2.4 except Versioning which is 2.2.6

--- OwinFramework.DefaultDocument/Properties/AssemblyInfo.cs
+++ OwinFramework.DefaultDocument/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.2.3")]
-[assembly: AssemblyFileVersion("2.2.3")]
+[assembly: AssemblyVersion("2.2.4")]
+[assembly: AssemblyFileVersion("2.2.4")]
